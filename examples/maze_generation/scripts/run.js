c56e7606c491df9508cf5a885d964190db98ee50
Stop <dbgusdn0922@naver.com>
2021-12-12 19:10:26 +0900

    edit example with engine

--- examples/maze_generation/scripts/run.js
+++ examples/maze_generation/scripts/run.js
@@ -1,19 +1,17 @@
-import { Engine } from "../../../you/engine.js";
-import { Screen } from "../../../you/screen.js";
+import { startEngine } from "../../../you/engine.js";
 import { MazeGeneration } from "./mazeGeneration.js";
 
 
-const SCREEN_WIDTH = 400;
-const SCREEN_HEIGHT = 400;
+const configurations = {
+    screens: {
+        default: {
+            canvas: document.querySelector('canvas'),
+            size: [800, 800],
+        }
+    },
+    applications: [
+        new MazeGeneration(),
+    ]
+}
 
-
-const engine = new Engine();
-
-engine.output.addScreen(
-    'default',
-    new Screen(document.querySelector('canvas'), SCREEN_WIDTH, SCREEN_HEIGHT)
-);
-
-engine.setApplication(new MazeGeneration());
-
-engine.start();
+startEngine(configurations);
