09ea3ad0b66b766549de56087f7fc0a525d12400
Stop <dbgusdn0922@naver.com>
2022-01-10 22:37:27 +0900

    add event utility

---
+++ you/utilities/event.js
@@ -0,0 +1,43 @@
+export class EventEmitter {
+
+    constructor() {
+        this.eventGroups = {};
+    }
+
+    on(event, listener, count=-1) {
+        if (count === 0) { return }
+
+        if (!(event in this.eventGroups)) {
+            this.eventGroups[event] = [];
+        }
+
+        this.eventGroups[event].push([listener, count]);
+    }
+
+    remove(event, listener=null) {
+        if (listener === null) {
+            delete this.eventGroups[event];
+        }
+        else if (event in this.eventGroups) {
+            const index = this.eventGroups[event].map(l => l[0]).indexOf(listener);
+
+            if (index >= 0) {
+                this.eventGroups[event].splice(index, 1);
+            }
+        }
+    }
+
+    emit(event, ...args) {
+        this.eventGroups[event]?.forEach(([listener, count], index) => {
+            if (count > 0) {
+                this.eventGroups[event][index][1] -= 1;
+            }
+            else if (count === 0) {
+                this.eventGroups[event].splice(index, 1);
+                return;
+            }
+
+            listener?.(...args);
+        });
+    }
+}
