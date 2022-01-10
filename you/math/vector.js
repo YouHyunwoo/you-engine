bdc83c1751bbf0d3780a67896396a4e323ddd6ae
Stop <dbgusdn0922@naver.com>
2022-01-10 21:53:25 +0900

    add operators in vector.

--- you/math/vector.js
+++ you/math/vector.js
@@ -1,3 +1,43 @@
+Array.prototype.add = function (other) {
+    if (other instanceof Array) {
+        if (this.length !== other.length) { throw Error() }
+        return this.map((value, index) => value + other[index]);
+    }
+    else {
+        return this.map(value => value + other);
+    }
+};
+
+Array.prototype.sub = function (other) {
+    if (other instanceof Array) {
+        if (this.length !== other.length) { throw Error() }
+        return this.map((value, index) => value - other[index]);
+    }
+    else {
+        return this.map(value => value - other);
+    }
+};
+
+Array.prototype.mul = function (other) {
+    if (other instanceof Array) {
+        if (this.length !== other.length) { throw Error() }
+        return this.map((value, index) => value * other[index]);
+    }
+    else {
+        return this.map(value => value * other);
+    }
+};
+
+Array.prototype.div = function (other) {
+    if (other instanceof Array) {
+        if (this.length !== other.length) { throw Error() }
+        return this.map((value, index) => value / other[index]);
+    }
+    else {
+        return this.map(value => value / other);
+    }
+};
+
 Array.prototype.contains = function (point) {
     if (this.length === 4) {
         if (point.length === 2) {
@@ -8,3 +48,8 @@ Array.prototype.contains = function (point) {
 
     throw Error();
 };
+
+Array.prototype.equals = function (other) {
+    if (this.length !== other.length) { throw Error() }
+        return this.every((v, i) => v === other[i]);
+}
