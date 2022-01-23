da659986a06544ffa0833a7bd31401acac388e57
Stop <dbgusdn0922@naver.com>
2022-01-23 20:59:37 +0900

    add object

--- you/object.js
+++ you/object.js
@@ -1,38 +1,97 @@
-export class BaseObject {
+import { Stateful } from "./framework/object.js";
 
-	constructor() {
+
+export class Object extends Stateful {
+
+	constructor(name, enable) {
+		super(enable);
+
+		this.name = name;
+		this.parent = null;
+		this.tags = new Set();
+		this.components = [];
 		this.objects = [];
 	}
 
-	create(...args) { // Call by Resource
+	create(...args) {
+		if (this._created) { return }
 		this.willCreate(...args);
+		this.event.emit('willCreate');
+		this._created = true;
+		this.components.forEach(component => component.create(...args));
 		this.objects.forEach(object => object.create(...args));
 		this.didCreate(...args);
+		this.event.emit('didCreate');
 	}
-	willCreate(...args) {}
-	didCreate(...args) {}
 
 	destroy(...args) {
+		if (!this._created || this._destroyed) { return }
 		this.willDestroy(...args);
+		this.event.emit('willDestroy');
+		this._created = false;
+		this.components.forEach(component => component.destroy(...args));
 		this.objects.forEach(object => object.destroy(...args));
 		this.didDestroy(...args);
+		this.event.emit('didDestroy');
 	}
-	willDestroy(...args) {}
-	didDestroy(...args) {}
 
 	update(deltaTime, events, input) {
+		if (!this._enabled || !this._created || this._destroyed) { return }
 		this.willUpdate(deltaTime, events, input);
+		this.components.forEach(component => component.update(deltaTime, events, input));
 		this.objects.forEach(object => object.update(deltaTime, events, input));
 		this.didUpdate(deltaTime, events, input);
 	}
-	willUpdate(deltaTime, events, input) {}
-	didUpdate(deltaTime, events, input) {}
 
-	render(context, screens) {
-		this.willRender(context, screens);
-		this.objects.forEach(object => object.render(context, screens));
-		this.didRender(context, screens);
+	render(context, screen, screens) {
+		if (!this._enabled || !this._created || this._destroyed) { return }
+		this.willRender(context, screen, screens);
+		this.components.forEach(component => component.render(context, screen, screens));
+		this.objects.forEach(object => object.render(context, screen, screens));
+		this.didRender(context, screen, screens);
 	}
-	willRender(screens) {}
-	didRender(screens) {}
+
+	add(object) {
+		this.objects.push(object);
+        object.parent = this;
+	}
+
+	remove(object) {
+		const index = this.objects.indexOf(object);
+
+        if (index >= 0) {
+            this.objects.splice(index, 1);
+            object.parent = null;
+        }
+	}
+
+	find(name) {
+		return this.objects.find(object => object.name === name) ?? null;
+	}
+
+	findAll(name) {
+		return this.objects.filter(object => object.name === name);
+	}
+
+	addComponent(component) {
+        this.components.push(component);
+        component.object = this;
+    }
+
+    removeComponent(component) {
+        const index = this.components.indexOf(component);
+
+        if (index >= 0) {
+            this.components.splice(index, 1);
+            component.object = null;
+        }
+    }
+
+	findComponent(type) {
+		return this.components.find(component => component instanceof type);
+	}
+
+	findAllComponent(type) {
+		return this.components.filter(component => component instanceof type);
+	}
 }
