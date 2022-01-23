bbf8d0248977b2b851c43708bacc2a6c85bdcac1
Stop <dbgusdn0922@naver.com>
2022-01-23 21:00:39 +0900

    apply new object structure and deal with objects in scene

--- you/scene.js
+++ you/scene.js
@@ -1,4 +1,4 @@
-import { BaseObject } from "./object.js";
+import { Base as BaseObject } from "./framework/object.js";
 
 
 export class Scene extends BaseObject {
@@ -7,6 +7,53 @@ export class Scene extends BaseObject {
         super();
 
         this.application = null;
+        this.objects = [];
+    }
+
+    create(...args) {
+		this.willCreate(...args);
+        this.objects.forEach(object => object.create(...args));
+		this.didCreate(...args);
+	}
+
+	destroy(...args) {
+		this.willDestroy(...args);
+        this.objects.forEach(object => object.destroy(...args));
+		this.didDestroy(...args);
+	}
+
+    update(deltaTime, events, input) {
+		this.willUpdate(deltaTime, events, input);
+        this.objects.forEach(object => object.update(deltaTime, events, input));
+		this.didUpdate(deltaTime, events, input);
+	}
+
+	render(context, screen, screens) {
+		this.willRender(context, screen, screens);
+        this.objects.forEach(object => object.render(context, screen, screens));
+		this.didRender(context, screen, screens);
+	}
+
+    add(object) {
+        this.objects.push(object);
+        object.parent = this;
+    }
+
+    remove(object) {
+        const index = this.objects.indexOf(object);
+
+        if (index >= 0) {
+            this.objects.splice(index, 1);
+            object.parent = null;
+        }
+    }
+
+    find(name) {
+        return this.objects.find(object => object.name === name);
+    }
+
+    findAll(name) {
+        return this.objects.filter(object => object.name === name);
     }
 
     push(scene, enterArgs) {
