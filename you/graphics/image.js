fe54235a98dce655a61d74f63e301e3c49946512
Stop <dbgusdn0922@naver.com>
2022-02-06 18:19:04 +0900

    add size properties in image

--- you/graphics/image.js
+++ you/graphics/image.js
@@ -5,9 +5,13 @@ export class Image {
 
         this.loaded = false;
         this.raw = new window.Image();
-        this.raw.onload = () => this.loaded = true;
+        this.raw.onload = () => { this.loaded = true }
         this.raw.src = url;
     }
+
+    get width() { return this.raw.width }
+    get height() { return this.raw.height }
+    get size() { return [this.raw.width, this.raw.height] }
 
     render(context, ...args) {
         if (this.loaded === false) { return }
