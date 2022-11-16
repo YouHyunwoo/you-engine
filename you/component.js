6ea43f5a706ab6aae58b5784a97624c11dc85144
Stop <dbgusdn0922@naver.com>
2022-11-16 23:51:27 +0900

    add constructor parameter to Component module.

--- you/component.js
+++ you/component.js
@@ -3,8 +3,14 @@ import { Enable as EnableObject } from "./framework/object.js";
 
 export class Component extends EnableObject {
 
-    constructor() {
-        super();
+    constructor({
+        enable=true,
+		events={},
+    }={}) {
+        super({
+            enable,
+			events,
+		});
 
         this.object = null;
     }
