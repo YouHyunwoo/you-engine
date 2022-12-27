781a1aab8cdc2bc67839f90f8c2f06c195c10017
Stop <dbgusdn0922@naver.com>
2022-12-27 15:50:24 +0900

    check if parent is available while updating and rendering in component module.

--- you/component.js
+++ you/component.js
@@ -14,4 +14,22 @@ export class Component extends EnableObject {
 
         this.object = null;
     }
+
+    update(deltaTime, events, input) {
+		if (this[this.constructor.ENABLE] && this.object?.created) {
+			this.willUpdate(deltaTime, events, input);
+			this.event.emit('willUpdate');
+			this.didUpdate(deltaTime, events, input);
+			this.event.emit('didUpdate');
+		}
+	}
+
+    render(context, screen, screens) {
+		if (this[this.constructor.ENABLE] && this.object?.created) {
+			this.willRender(context, screen, screens);
+			this.event.emit('willRender');
+			this.didRender(context, screen, screens);
+			this.event.emit('didRender');
+		}
+	}
 }
