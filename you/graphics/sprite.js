c940d0c46391305cb57837823f6467343da135da
Stop <dbgusdn0922@naver.com>
2022-12-31 03:50:08 +0900

    add area property and modify rendering method, enable serialization in sprite module.

--- you/graphics/sprite.js
+++ you/graphics/sprite.js
@@ -1,4 +1,3 @@
-import resource from "../utilities/resource.js";
 import { Image } from "./image.js";
 
 
@@ -7,38 +6,71 @@ export class Sprite {
     constructor({
         sheet,
         scale=[1, 1], anchor=[0, 0],
-        clip=null
+        croppingArea=null,
     }={}) {
+        if (!sheet) { throw 'required' }
+
         this.sheet = sheet;
         this.scale = scale;
         this.anchor = anchor;
-        this.clip = clip;
+        this.croppingArea = croppingArea;
     }
 
-    render(context, x, y) {
-        if (!this.sheet) { return }
+    get area() {
+        const size = this.croppingArea?.slice(2, 4) ?? this.sheet.size;
+        const scaledSize = size.mul(this.scale);
+        const scaledPosition = scaledSize.mul(this.anchor).negate;
 
+        return [...scaledPosition, ...scaledSize];
+    }
+
+    render(context, x=0, y=0) {
+        if (this.sheet.loaded) {
+            this._drawSprite(context, x, y);
+        }
+    }
+
+    _drawSprite(context, x, y) {
         context.save();
         context.translate(x, y);
         context.scale(...this.scale);
-        if (this.clip) {
-            context.translate(-this.anchor[0] * this.clip[2],
-                -this.anchor[1] * this.clip[3]);
-            this.sheet?.render(context, ...this.clip, 0, 0, ...this.clip.slice(2, 4));
+
+        if (this.croppingArea) {
+            context.translate(
+                -this.anchor[0] * this.croppingArea[2],
+                -this.anchor[1] * this.croppingArea[3]
+            );
+            this.sheet.render(
+                context,
+                ...this.croppingArea,
+                0, 0, this.croppingArea[2], this.croppingArea[3]
+            );
         }
         else {
-            context.translate(-this.anchor[0] * this.sheet.width,
-                -this.anchor[1] * this.sheet.height);
-            this.sheet?.render(context, 0, 0);
+            context.translate(
+                -this.anchor[0] * this.sheet.width,
+                -this.anchor[1] * this.sheet.height
+            );
+            this.sheet.render(
+                context,
+                0, 0
+            );
         }
+
         context.restore();
     }
 
-    static async load(url) {
-        const data = await resource.json.load(url);
+    toJSON() {
+        return {
+            sheet: this.sheet.url,
+            scale: this.scale,
+            anchor: this.anchor,
+            croppingArea: this.croppingArea,
+        };
+    }
 
-        data.sheet = new Image(data.sheet);
-
-        return new this(data);
+    static fromJSON(obj) {
+        obj.sheet = new Image(obj.sheet);
+        return new this(obj);
     }
 }
