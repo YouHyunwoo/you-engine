de1f9e50f3b59b70b79f639e55abda8c04f011c3
Stop <dbgusdn0922@naver.com>
2023-01-07 14:51:33 +0900

    add scaling of camera for rendering in scene module.

--- you/scene.js
+++ you/scene.js
@@ -59,6 +59,7 @@ export class Scene extends Stateful {
             if (camera) {
                 context.save();
                 context.translate(screen.width / 2, screen.height / 2);
+                context.scale(...camera.scale);
                 context.translate(-Math.floor(camera.position[0]), -Math.floor(camera.position[1]));
             }
 
