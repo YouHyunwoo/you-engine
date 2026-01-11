2819215df5dafa3160e449aedcd4376243df467a
kvjng <fkrfkrdk@gmail.com>
2026-01-11 18:32:34 +0900

    feat(examples): mini-rpg 프로젝트 기초 구조 설정

---
+++ examples/mini-rpg/scripts/game.js
@@ -0,0 +1,7 @@
+import { SceneApplication } from '../../../you/application.js'
+
+export class MiniRPG extends SceneApplication {
+  didCreate() {
+    console.log('MiniRPG started')
+  }
+}
---
+++ examples/mini-rpg/scripts/main.js
@@ -0,0 +1,12 @@
+import { You } from '../../../you/you.js'
+import { MiniRPG } from './game.js'
+
+You.run({
+  screens: {
+    main: {
+      canvas: document.querySelector('#game'),
+      size: [800, 600]
+    }
+  },
+  applications: [new MiniRPG({ mainScreen: 'main' })]
+})
